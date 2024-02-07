9f40a9b6685c506cf21bc484d186965e89acf1c1
Raunak Anand <46580556+raunak26@users.noreply.github.com>
2024-02-07 13:30:45 +0000

    added message for Vidhi and made css changes

--- src/App.tsx
+++ src/App.tsx
@@ -38,18 +38,46 @@ function App() {
       {yesPressed ? (
         <>
           <img
+            className="bear-kissing"
             alt="bears kissing"
             src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif"
           />
-          <div className="text">Ok yay!!!</div>
+          <div className="text1">YAYYYYY!!!</div>
+          <p className="text2">
+            To my favoritest person in the world, <br />
+            <br />
+            I love you to the center of the multiverse and back. Thought I'd
+            show you much I love you through "code". I hoped you liked it
+            because it's now part of my resume projects. <br />
+            <br />
+            "So tell me about a project you're proud" <br />
+            "I had to ask my girlfriend to be my valentine and had to make sure
+            saying NO was not an option"
+            <br />
+            "YOU'RE HIRED!"
+            <br />
+            <br />I believe this is my seventh year with you as my valentine and
+            yus, I am truly honored. Thank you for being my best friend, my
+            partner in crime, and the love of my life. I love you more than you
+            love me.
+            <br />
+            <br />
+            Love,
+            <br />
+            Your Ron
+          </p>
         </>
       ) : (
         <>
           <img
+            className="bear-heart"
             alt="bear with hearts"
             src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
           />
-          <div>Will you be my Valentine?</div>
+          <div className="text1">Dawu, will you be my Valentine?</div>
+          <div className="text3">
+            (Try saying no, I dare you. No but actually try it hehe.)
+          </div>
           <div>
             <button
               className="yesButton"
